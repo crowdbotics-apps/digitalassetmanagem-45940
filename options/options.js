642a6cabf03c57e1b3b9a8c102b29b3e76b6118d
Crowdbotics <team@crowdbotics.com>
2024-02-05 17:30:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "digitalassetmanagem-45940",
+  url: "https://digitalassetmanagem-45940.botics.co",
+  api: "https://digitalassetmanagem-45940.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
